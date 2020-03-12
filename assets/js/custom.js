698f339d9894ef7564a46da032e63a4919e0150e
JingTingZeng <kelly20829tw@gmail.com>
2020-03-12 14:39:03 +0800

    1. add social link 2.add nav scroll animation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,17 @@
 (function(){
     $("#form-warning").hide();
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
     var mySwiper = new Swiper ('#project-slide', {
         navigation: {
             nextEl: '.collection-button-next',
